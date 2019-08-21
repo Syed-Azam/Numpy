ca367ebdf0a2b80f309c92d7e9388d7b2dc17655
Syed Azam Hassan <kashif.hassan@gmail.com>
2019-08-21 10:29:43 +0500

    Pandas & Numpy

--- numpy06.py
+++ numpy06.py
@@ -3,5 +3,5 @@
 import numpy as np
 a = np.array([[1,2,3],[1,2,3]])
 b = np.array([[2,2],[2,2],[3,3]])
-print("Dot Product : ", np.dot(a,b)) # numpy dot product
+print("Dot Product : ", np.dot(a,b)) ## numpy dot product
 
